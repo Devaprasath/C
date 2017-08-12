6bbbb6a947616a871e54d9d0b229a9091581efe9
Devaprasath <30952697+Devaprasath@users.noreply.github.com>
2017-08-12 17:05:15 +0530

    positive or not

---
+++ positiveornot.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("enter the value of a:"\n);
+scanf("%d",&a);
+if(a>0)
+printf("the number a is positive");
+else
+printf("the number a is negative");
+else
+printf("neither positive nor negative");
+getch();
+}
